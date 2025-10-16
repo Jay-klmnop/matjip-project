3d2d1c8da0b0eb0e6a4b22fca69cb055f6ee564b
DESKTOP-SPQ41UM\user <jiyeyoun@outlook.com>
2025-10-16 14:10:25 +0900

    style: Import Google font, apply to index.css and style headings with custom class

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 function App() {
-  
-
   return (
     <>
-      
+      <section>
+        <h1 className="heading">찜한 목록</h1>
+      </section>
+      <section>
+        <h1 className="heading">맛집 리스트</h1>
+      </section>
     </>
   )
 }
