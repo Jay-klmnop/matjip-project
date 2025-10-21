aa3068f5a42834c2b6499189de7d4c35f1dd0214
Jay <jiyeyoun@outlook.com>
2025-10-21 17:16:09 +0900

    style: Update font to improve readability and fix layout spacing
    Closes #32

--- src/components/MatjipCard.tsx
+++ src/components/MatjipCard.tsx
@@ -9,7 +9,7 @@ interface MatjipCardProps {
 
 export function MatjipCard({ matjip, isLiked, onToggleLiked }: MatjipCardProps) {
   return (
-    <div className='card flex max-w-md min-w-52 flex-col items-center gap-4 rounded-lg p-4 shadow-md transition-opacity duration-300 ease-in-out'>
+    <div className='card flex max-w-md min-w-52 flex-col items-center gap-2 rounded-md p-4 shadow-md transition-opacity duration-300 ease-in-out'>
       <figure className='relative h-56 w-full overflow-hidden rounded-lg'>
         <div className='like-bg absolute top-0 right-0 z-50 flex items-start justify-end pt-2 pr-2'>
           <LikeIcon isLiked={isLiked} onClick={onToggleLiked} />
@@ -23,7 +23,7 @@ export function MatjipCard({ matjip, isLiked, onToggleLiked }: MatjipCardProps) 
       </figure>
       <div className='flex w-full flex-col gap-2 text-left'>
         <h3 className='card-heading text-left'>{matjip.title}</h3>
-        <p>{matjip.description}</p>
+        <p className='leading-5'>{matjip.description}</p>
       </div>
     </div>
   );
